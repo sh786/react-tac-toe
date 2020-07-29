3cdee8dac17e3480503346432bcd9c108c9ff421
Sam Hamburger <samhamburger@MacBook-Pro-2.lan1>
2020-07-29 14:18:17 -0400

    Updates

---
+++ precache-manifest.94b2258696308d62f4ea3d26e4557203.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c3f056f16873876775109c823345b3",
+    "url": "/react-tac-toe/index.html"
+  },
+  {
+    "revision": "d37d970b35e33762202c",
+    "url": "/react-tac-toe/static/css/main.b49b190a.chunk.css"
+  },
+  {
+    "revision": "3142249d8202402ecf3e",
+    "url": "/react-tac-toe/static/js/2.cc329a73.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-tac-toe/static/js/2.cc329a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37d970b35e33762202c",
+    "url": "/react-tac-toe/static/js/main.c5624734.chunk.js"
+  },
+  {
+    "revision": "eaf86387f512c9d2bb73",
+    "url": "/react-tac-toe/static/js/runtime-main.89f73e65.js"
+  },
+  {
+    "revision": "761eacd17881188fcb2300f4ec68718e",
+    "url": "/react-tac-toe/static/media/bug.761eacd1.svg"
+  },
+  {
+    "revision": "65ab40d9cecf2d6b63f131a9492ddafd",
+    "url": "/react-tac-toe/static/media/trophy.65ab40d9.svg"
+  }
+]);
